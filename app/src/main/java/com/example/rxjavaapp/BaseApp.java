accd7310ac0a02a3bcee788e48a614073cf1c6c6
Chigozie.Asadu <chigozie.asau@globalaccelerex.com>
2022-01-13 20:14:58 +0100

    Migrate to Dagger hilt from Dagger2

--- app/src/main/java/com/example/rxjavaapp/BaseApp.java
+++ app/src/main/java/com/example/rxjavaapp/BaseApp.java
@@ -1,31 +1,26 @@
-//package com.example.rxjavaapp;
-//
-//import android.app.Application;
-//import com.example.rxjavaapp.di.DaggerApiComponent;
-//import com.example.rxjavaapp.di.ApiComponent;
-//import com.example.rxjavaapp.di.ApiModule;
-//
-//
-//
-//public class BaseApp extends Application {
-//
-//    private static BaseApp baseApp;
-//    private ApiComponent apiComponent;
-//
-//    public static BaseApp getBaseApp() {
-//        return baseApp;
-//    }
-//
-//    @Override
-//    public void onCreate() {
-//        super.onCreate();
-//        baseApp = this;
-//        apiComponent = DaggerApiComponent.builder()
-//                .apiModule(new ApiModule())
-//                .build();
-//    }
-//
-//    public ApiComponent getApiComponent() {
-//        return apiComponent;
-//    }
-//}
+package com.example.rxjavaapp;
+
+
+import android.app.Application;
+
+import dagger.hilt.EntryPoint;
+import dagger.hilt.android.HiltAndroidApp;
+
+
+@HiltAndroidApp()
+public class BaseApp extends Application {
+
+    private static BaseApp baseApp;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    public static BaseApp getBaseApp() {
+        return baseApp;
+    }
+
+
+}
